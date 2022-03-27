93226edadf9669439cbc91e3a2a65920e49d1822
Sixtus Nosike <sixtuswork18@gmail.com>
2022-03-27 09:50:26 +0200

    index-side: popular

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,9 +38,36 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader ()
+{
+    const header = document.getElementById('header');
+    //Cuando el scroll es mayor que 50 de la altura de la pantalla, añade la clase scroll-header
+    this.scrollY >= 50 ? header.classList.add('scroll-header'):header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
+//_________________________________________________________//
 
 /*=============== POPULAR SWIPER ===============*/
+let swiperPopular = new Swiper(".popular__container", {
+    loop: true,
+    spaceBetween: 24,
+    slidesPerView : 'auto',
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+    breakpoints: {
+    768: {
+        slidesPerView: 3,
+        },
+
+    1024: {
+        spaceBetween: 48,
+        },
+
+    },
+});
 
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
