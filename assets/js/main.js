39fc92eea637432f373a4d12f0890c87a4e38dc0
Sixtus Nosike <sixtuswork18@gmail.com>
2022-03-29 22:04:47 +0200

    mobile-first

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,15 +71,75 @@ let swiperPopular = new Swiper(".popular__container", {
 
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
-
+const mixerFeatured = mixitup('.featured__content', {
+    selectors: {
+        target: '.featured__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
 /* Link active featured */ 
+const linkFeatured = document.querySelectorAll('.featured__item');
 
+function activeFeatured ()
+{
+    linkFeatured.forEach(n => n.classList.remove('active-featured'));
+    this.classList.add('active-featured');
+}
+
+linkFeatured.forEach(l => l.addEventListener('click', activeFeatured));
 
 /*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp ()
+{
+    const scrollUp = document.getElementById('scroll-up');
+
+    if(this.scrollY >= 350) 
+    {
+        scrollUp.classList.add('show-scroll');
+    }else
+    {
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+
+window.addEventListener('scroll', scrollUp);
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const section = document.querySelectorAll('section[id]');
 
+function scrollActive()
+{
+    const scrollY = window.pageYOffset;r
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id');
+
+            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
+            {
+                document.querySelector('.nav__menu a[href *=' + sectionId + ']').classList.add('active-link');
+            }else
+            {
+                document.querySelector('.nav__menu a[href *=' + sectionId + ']').classList.remove('active-link');
+            }
+    })
+}
+
+window.addEventListener('scroll', scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+
+// function activeStateColor(items, action, prop)
+// {
+//     const linkStateColor = document.querySelectorAll(items);
+//     linkFeatured.forEach(l => l.classList.remove(prop));
+//     this.classList.add(prop);
+//     linkStateColor.forEach(l => l.addEventListener (action, activeStateColor));
+// }
+// activeStateColor('.featured__item', 'click', 'active-featured');
