d2183d31eefe4fb0de8ff16afee744c7cdcd3c0f
Sixtus Nosike <sixtuswork18@gmail.com>
2022-03-31 19:04:44 +0200

    responsive: page

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,7 +113,7 @@ const section = document.querySelectorAll('section[id]');
 
 function scrollActive()
 {
-    const scrollY = window.pageYOffset;r
+    const scrollY = window.pageYOffset;
 
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
@@ -122,10 +122,10 @@ function scrollActive()
 
             if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
             {
-                document.querySelector('.nav__menu a[href *=' + sectionId + ']').classList.add('active-link');
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
             }else
             {
-                document.querySelector('.nav__menu a[href *=' + sectionId + ']').classList.remove('active-link');
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
             }
     })
 }
